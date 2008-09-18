dd2f4ce27aa84a5c07f341673aaaf5db5ad31343
eben.howard <eben.howard@f418faf6-7ec1-11dd-9205-735403a6da14>
2008-09-18 23:01:17 +0000

    In progress removal of MonsterTypeEnum, currently program is broken.

--- MonsterObject.java
+++ MonsterObject.java
@@ -20,13 +20,47 @@ public class MonsterObject extends BaseObject {
          defense, //base defense against attack
          satiation, //how full player will get from absorbing the monster
          x,  y; //current x,y coordinates for the monster
-    private MonsterTypeEnum myType;
     private Vector<FeelingsObject> feelings = null;
     private boolean awake = false; //will tell us if the monster is active, default is not active
+    
+    /* The following constants are to make default monsters
+     * of the listed types
+     */
+      public static final MonsterObject TRUTH = new MonsterObject("truth quark",'t',false,CSIColor.MAGENTA,100,100,50,50,10), 
+    BEAUTY = new MonsterObject("beauty quark",'b',false,CSIColor.GREEN,100,50,100,50,10),
+    CHARM = new MonsterObject("charm quark",'c',false,CSIColor.GRAY,50,50,25,20,5),
+    STRANGE = new MonsterObject("strange quark",'s',false,CSIColor.CYAN,50,25,50,20,5),
+    UP = new MonsterObject("up quark",'u',false,CSIColor.YELLOW,5,2,1,2,1),
+    DOWN = new MonsterObject("down quark",'d',false,CSIColor.BLUE,5,1,2,2,1),
+    ANTIUP = new MonsterObject("antiup quark", 'u',false,CSIColor.YELLOW,5,2,1,2,1);
 
     MonsterObject() {
         super();
-        myType = MonsterTypeEnum.UP;
+    }
+    
+    
+    public MonsterObject(MonsterObject mon) {
+        this.myType = myType;
+        this.myName = myType.myName;
+        this.represent = myType.represent;
+        this.passable = myType.passable;
+        this.myColor = myType.myColor;
+        this.hp = myType.hp;
+        this.damage = myType.damage;
+        this.attack = myType.attack;
+        this.defense = myType.defense;
+        this.satiation = myType.satiation;
+    }
+    
+    //this constructor lets us set everything, probably useful for unique monsters
+    public MonsterObject(String myName, char represent, boolean passable, CSIColor myColor, int hp, int damage, int attack, int defense, int satiation) {
+        super(myName, represent, passable, myColor);
+        this.hp = hp;
+        this.damage = damage;
+        this.attack = attack;
+        this.defense = defense;
+        this.satiation = satiation;
+        this.feelings = new Vector<FeelingsObject>();
     }
 
     public int getDefense() {
@@ -53,10 +87,6 @@ public class MonsterObject extends BaseObject {
         return satiation;
     }
 
-    public MonsterTypeEnum getType() {
-        return myType;
-    }
-
     public Boolean getWakeful() {
         return awake;
     }
@@ -69,36 +99,13 @@ public class MonsterObject extends BaseObject {
         awake = false;
     }
 
-    public MonsterObject(MonsterTypeEnum myType) {
-        this.myType = myType;
-        this.myName = myType.myName;
-        this.represent = myType.represent;
-        this.passable = myType.passable;
-        this.myColor = myType.myColor;
-        this.hp = myType.hp;
-        this.damage = myType.damage;
-        this.attack = myType.attack;
-        this.defense = myType.defense;
-        this.satiation = myType.satiation;
-    }
-    //this constructor lets us set everything, probably useful for unique monsters
-    public MonsterObject(String myName, char represent, boolean passable, CSIColor myColor, int hp, int damage, int attack, int defense, int satiation) {
-        super(myName, represent, passable, myColor);
-        this.hp = hp;
-        this.damage = damage;
-        this.attack = attack;
-        this.defense = defense;
-        this.satiation = satiation;
-        this.feelings = new Vector<FeelingsObject>();
-    }
-
     @Override
     public String objectOutput() {// this should be overridden to ensure everything is saved correctly
         String ret = "";
         String eol = System.getProperty("line.separator");
         ret = "MonsterObject" + eol + myName + eol + String.valueOf(represent) + eol + String.valueOf(passable) + eol + myColor.getColor() + eol + String.valueOf(visible) + eol 
             + hp + eol + damage + eol + attack + eol + defense + eol + satiation + eol 
-            + x + eol + y + eol + myType.myName + eol + String.valueOf(awake) + eol + eol;
+            + x + eol + y + eol + String.valueOf(awake) + eol + eol;
 
 
 //    private Vector<FeelingsObject> feelings = null; needs to be added when feelings are added
@@ -120,7 +127,6 @@ public class MonsterObject extends BaseObject {
             satiation = Integer.valueOf(reader.readLine());
             x = Integer.valueOf(reader.readLine());
             y = Integer.valueOf(reader.readLine());
-            myType = myType.figureType(reader.readLine());
             awake = reader.readLine().equalsIgnoreCase("true");
         } catch (IOException ioe) {
             System.out.println("Fatal error reading from file!");
